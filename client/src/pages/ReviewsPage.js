cf203271f77bf6d09c62bdccb63a16fa984ab3bb
Kevin Li <likevinhp@gmail.com>
2023-08-27 02:43:36 +0000

    Changed review page to table form

--- client/src/pages/ReviewsPage.js
+++ client/src/pages/ReviewsPage.js
@@ -29,47 +29,73 @@ export default function ReviewsPage({ onCreate, onEdit }) {
   if (!reviews) return null;
 
   return (
-    <>
-      <div className="review-rating-label">
+    <div className="reviews-container">
+      {/* <div className="review-rating-label">
         <h1 className="item-label"></h1>
         <h1 className="rating-label">Rating</h1>
         <h1 className="review-label">Review</h1>
-      </div>
-
-      <div className="reviews-container">
-        <ul className="rowOfReviews">
-          {reviews.map((review) => (
-            <Review item={review} />
-          ))}
-        </ul>
-      </div>
-    </>
+      </div> */}
+      <table>
+        <tr>
+          <th scope="col"></th>
+          <th scope="col">
+            <h1>Rating</h1>
+          </th>
+          <th scope="col">
+            <h1>Review</h1>
+          </th>
+        </tr>
+        {reviews.map((review) => (
+          <Review item={review} />
+        ))}
+      </table>
+    </div>
   );
 }
 
 function Review({ item }) {
   const { title, rating, review, imageUrl, itemId } = item;
   return (
-    <li class="review-container" key={itemId}>
-      <div className="r-image-container">
-        <Link to={`/animeDetails/${itemId}`} className="link">
-          <div className="review-icon-title-container">
-            <img src={imageUrl} className="review-image" alt={title} />
-            <h5 className="review-title">
-              <span className="span-title">{title}</span>
-            </h5>
-          </div>
-        </Link>
-      </div>
-
-      <div className="r-rating-container">
-        {/* <h1 className="rating-label">Rating</h1> */}
+    // <li class="review-container" key={itemId}>
+    <tr>
+      <td className="image-test">
+        <div className="r-image-container">
+          <Link to={`/animeDetails/${itemId}`} className="link">
+            <div className="review-icon-title-container">
+              <img src={imageUrl} className="review-image" alt={title} />
+              <h5 className="review-title">
+                <span className="span-title">{title}</span>
+              </h5>
+            </div>
+          </Link>
+        </div>
+      </td>
+      <td className="rating-test">
         <h1 className="review-rating">{rating}</h1>
-      </div>
-      <div className="r-review-container">
-        {/* <h1 className="review-label">Review</h1> */}
+      </td>
+      <td className="review-test">
         <h3 className="r-review">{review}</h3>
-      </div>
-    </li>
+      </td>
+    </tr>
+    // <div className="r-image-container">
+    //   <Link to={`/animeDetails/${itemId}`} className="link">
+    //     <div className="review-icon-title-container">
+    //       <img src={imageUrl} className="review-image" alt={title} />
+    //       <h5 className="review-title">
+    //         <span className="span-title">{title}</span>
+    //       </h5>
+    //     </div>
+    //   </Link>
+    // </div>
+
+    // <div className="r-rating-container">
+    //   {/* <h1 className="rating-label">Rating</h1> */}
+    //   <h1 className="review-rating">{rating}</h1>
+    // </div>
+    // <div className="r-review-container">
+    //   {/* <h1 className="review-label">Review</h1> */}
+    //   <h3 className="r-review">{review}</h3>
+    // </div>
+    // </li>
   );
 }
